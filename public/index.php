0f92265f2c87729eee76fd7b6646ca45251ad99f
Mariusz Stróż <m.stroz@codelines.io>
2025-02-01 13:48:11 +0100

    feat: Add valitron and dotenv

--- public/index.php
+++ public/index.php
@@ -9,17 +9,25 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/..");
+$dotenv->load();
+
 $container = new Container();
 AppFactory::setContainer($container);
 
 $app = AppFactory::create();
+$app->addBodyParsingMiddleware();
+
 $app->addErrorMiddleware(true, true, true)
     ->getDefaultErrorHandler()
     ->forceContentType('application/json');
 
 $app->add(new JsonResponseHeader());
 
-$app->get('/track/visit', TrackController::class . ':visit');
-$app->get('/track/visit/{id:[0-9]+}', TrackController::class . ':visitOne');
+$app->group('/track', function ($app) {
+    $app->get('/visit', TrackController::class . ':visit');
+    $app->get('/visit/{id:[0-9]+}', TrackController::class . ':one');
+    $app->post('/save', TrackController::class . ':save');
+});
 
 $app->run();
--- src/App/Controllers/TrackController.php
+++ src/App/Controllers/TrackController.php
@@ -7,12 +7,19 @@ namespace App\Controllers;
 use App\TestService;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Valitron\Validator;
 
 class TrackController
 {
 
-    public function __construct(public TestService $testService)
-    {
+    public function __construct(
+        public TestService $testService,
+        public Validator $validator
+    ) {
+
+        $this->validator->mapFieldsRules([
+            'name' => ['required','slug'],
+        ]);
     }
 
     public function visit(Request $request, Response $response, array $args): Response
@@ -22,11 +29,24 @@ class TrackController
         return $response;
     }
 
-    public function visitOne(Request $request, Response $response, array $args): Response
+    public function one(Request $request, Response $response, array $args): Response
     {
         $body = json_encode(['id' => $args['id']]);
         $response->getBody()->write($body);
         return $response;
     }
 
+    public function save(Request $request, Response $response, array $args): Response
+    {
+        $body = $request->getParsedBody();
+
+        if (!$this->validator->withData($body)->validate()) {
+            $response->getBody()->write(json_encode($this->validator->errors()));
+            return $response->withStatus(422);
+        }
+
+        $html = var_export($body, true);
+        $response->getBody()->write($_ENV['MONGO_URI']);
+        return $response;
+    }
 }
